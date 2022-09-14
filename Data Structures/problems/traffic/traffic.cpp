bda6538e8270174c9e51719a9a439608b2b12d56
Xingrui <xingruichen2@gmail.com>
2022-09-14 11:13:44 -0700

    cses traffic lights didn't solve yet 3x AC 2x WA 7x TLE

---
+++ Data Structures/problems/traffic/traffic.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+int x, n;
+int p[200001];
+map<int, int> pass; // key is where the passage starts, value is the length of the passage
+int main() {
+    cin >> x >> n;
+    for (int i = 1; i <= n; i++) {
+        cin >> p[i];
+    }
+    pass[1] = x;
+    for (int i = 1; i <= n; i++) {
+        // for (auto i : pass) {
+        //     cerr << i.first << ' ' << i.second << endl;
+        // }
+        // cerr << endl;
+
+        auto t = pass.upper_bound(p[i]);
+        t--;
+        int start = (*t).first;
+        if (p[i] < start + (*t).second - 1)
+            pass[p[i] + 1] = (*t).second - (p[i] - start + 1);
+        if (start != p[i]) {
+            t->second = p[i] - start + 1;
+        }
+
+        // find the largest passage and output it
+        int m = 0;
+        for (auto i : pass) {
+            m = max(m, i.second);
+        }
+        cout << m;
+        if (i < n)
+            cout << ' ';
+        else
+            cout << endl;
+    }
+    // for (auto i : pass) {
+    //     cerr << i.first << ' ' << i.second << endl;
+    // }
+    // cerr << endl;
+}
