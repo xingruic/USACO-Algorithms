8e47c55aba8573d397547063928bf8314f784d8f
xingruic <xingruichen2@gmail.com>
2023-07-28 16:34:23 -0700

    email filing

--- Simulation/problems/email_filing/email.cpp
+++ Simulation/problems/email_filing/email.cpp
@@ -1,54 +1,30 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-int t;
 int m, n, k;
 vector<int> f;
-int last(int minf, int maxf) {
-    for (int i = f.size() - 1; i > 0; i--) {
-        if (f[i] >= minf && f[i] < maxf)
-            return i;
-    }
-    return 0x7fffffff;
-}
 int main() {
+    freopen("email.in", "r", stdin);
     freopen("email.out", "w", stdout);
+    int t;
     cin >> t;
-    for (int tt = 0; tt < t; tt++) {
-        f.clear();
-        f.push_back(0);
+    while (t--) {
         cin >> m >> n >> k;
-        for (int i = 1; i <= n; i++) {
-            int x;
-            cin >> x;
-            f.push_back(x);
+        f.resize(n, 0);
+        for (int i = 0; i < n; i++) {
+            cin >> f[i];
+            f[i]--; // remember to add back
         }
-        int posn = 1;
-        for (int fol = 1; fol <= m; fol += k) {
-            int efol = min(fol + k, m + 1);
-            int cur;
-            int prev = 0x7fffffff;
-            while (posn < f.size() && last(fol, efol) != 0x7ffffff) {
-                while (prev > (cur = last(fol, efol))) {
-                    prev = cur;
-                    for (int i = posn; i < posn + k; i++) {
-                        while (i < f.size() && f[i] >= fol && f[i] < efol) {
-                            f.erase(f.begin() + i);
-                        }
-                    }
+        int bottom = k;
+        while (bottom > 0) {
+            for (int i = bottom - k; i < n && i < bottom; i++) {
+                if (f[i] >= bottom - k && f[i] < bottom) {
+                    f.erase(next(f.begin(), i));
+                    i--, n--;
                 }
-                posn++;
             }
-            if (posn > f.size())
-                posn = f.size() - 1;
+            bottom++;
+            if (bottom > n) bottom = n;
         }
-        cout << "posn " << posn << endl;
-        for (int i = 1; i < f.size(); i++) {
-            cout << f[i] << ' ';
-        }
-        cout << endl;
-        if (f.size() > 0)
-            cout << "NO" << endl;
-        else
-            cout << "YES" << endl;
+        for (int &i : f) cout << i << endl;
     }
 }
