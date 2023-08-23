f6bfe8b8622f2e5dcb1f699cf2ee0cd3ebe4ca3f
xingruic <xingruichen2@gmail.com>
2023-08-22 17:41:14 -0700

    build gates (barely started)

---
+++ Flood Fill/problems/build_gates/gates.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n;
+string s;
+int main() {
+    freopen("gates.in", "r", stdin);
+    freopen("gates.out", "w", stdout);
+    cin >> n >> s;
+
+}
